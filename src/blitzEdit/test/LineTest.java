f32348b7f2cc1e69fb0f03f8b1e6a8efa654cded
David Schick <david.schick@online.de>
2017-05-18 14:42:02 +0200

    added test for Circuit.getLine() method

---
+++ src/blitzEdit/test/LineTest.java
@@ -0,0 +1,40 @@
+package blitzEdit.test;
+
+import java.util.ArrayList;
+
+import blitzEdit.core.*;
+
+public class LineTest implements Runnable {
+
+	@Override
+	public void run() 
+	{
+
+		ArrayList<Element> elements = new ArrayList<Element>();
+		int [][] relPos = {{0, 10},{0, -10}};
+		short [] relRot = {0, 0};
+		
+		Component source = new Component(0, 0, (short)0, "Source", relPos, relRot, new String());
+		Component resistor = new Component(100, 100, (short)0, "Resistor", relPos, relRot, new String());
+		Component coil = new Component(200, 100, (short)0, "Coil", relPos, relRot, new String());
+		
+		elements.add(source);
+		elements.add(coil);
+		elements.add(resistor);
+		
+		Circuit circuit = new Circuit(elements, "Schaltplan 1");
+		
+		source.getConnectors().get(0).connect(resistor.getConnectors().get(0));
+		source.getConnectors().get(1).connect(coil.getConnectors().get(0));
+		coil.getConnectors().get(1).connect(resistor.getConnectors().get(1));
+		
+		for ( Line l : circuit.getLines())
+		{
+			System.out.println(l.getP1() + " ---> " + l.getP2());
+		}
+		
+	}
+	
+	public LineTest()
+	{}
+}
