c9b6a2d18ead98270c94bf870bf7a4daea6b4768
David Schick <david.schick@online.de>
2017-06-04 17:55:45 +0200

    added javadoc to ComponentBlueprint.java

--- src/blitzEdit/core/ComponentBlueprint.java
+++ src/blitzEdit/core/ComponentBlueprint.java
@@ -3,41 +3,71 @@ package blitzEdit.core;
 import java.util.ArrayList;
 import java.util.List;
 
-// Class ComponentBlueprint:
-// holds constructor parameters for a
-// specific component.
+/**
+ * Defines an archetype for a Component.
+ * Used by the {@link Component} constructor to make sure components of the same type
+ * are constructed equally
+ * @author David Schick
+ */
 public class ComponentBlueprint 
 {
+	/**
+	 * @return typename of component
+	 */
 	public String getType()
 	{
 		return new String(_type);
 	}
 	
+	/**
+	 * @return path to svg-image assigned to this blueprint
+	 */
 	public String getSvgFilePath()
 	{
 		return new String(_svgFilePath);
 	}
-	
+	/**
+	 * @return array holding the relative positions of the components connectors
+	 */
 	public int [][] getRelPos() 
 	{
 		return _conRelPos.clone();
 	}
 	
+	/**
+	 * @return array holding the relative rotation of this components connectors
+	 */
 	public short [] getConRelRot()
 	{
 		return _conRelRot.clone();
 	}
 	
+	/**
+	 * @return x-size of component
+	 */
 	public int getSizeX()
 	{
 		return _sizeX;
 	}
 	
+	/**
+	 * @return y-size of component
+	 */
 	public int getSizeY()
 	{
 		return _sizeY;
 	}
 	
+	/**
+	 * Constructs new ComponentBlueprint
+	 * @param type typename for {@link Component component}
+	 * @param svgFilePath path to svg-image assigned to this {@link Component component}
+	 * @param relPos array, holding the relative positions of the {@link Connector connectors} of this component
+	 * @param conRelRot  array, holding the relative rotations of the {@link Connector connectors} of this component
+	 * @param sizeX x-size of {@link Component component}
+	 * @param sizeY y-size of {@link Component component} 
+	 * @param properties list of all {@link ComponentProperty properties}
+	 */
 	public ComponentBlueprint(String type, String svgFilePath, int [][] relPos, 
 							short [] conRelRot, int sizeX, int sizeY, List<ComponentProperty> properties)
 	{
